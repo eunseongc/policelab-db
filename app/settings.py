173dc002a21b023a8205919f5625051dcb72b333
Sangwoo Han <realappsdev@gmail.com>
2020-01-11 18:05:16 +0900

    set channel_redis

--- app/settings.py
+++ app/settings.py
@@ -197,6 +197,9 @@ ASGI_APPLICATION = "app.asgi.application"
 
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels.layers.InMemoryChannelLayer",
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("redis", 6379)],
+        },
     },
 }
