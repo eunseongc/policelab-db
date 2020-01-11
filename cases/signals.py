cd27cb9497d96be5e7b6b92e3f54a8e952d6951d
Sangwoo Han <realappsdev@gmail.com>
2020-01-11 18:07:11 +0900

    Add post_save and post_delete signal for Video model

---
+++ cases/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save, post_delete
+from graphene_subscriptions.signals import (post_save_subscription,
+                                            post_delete_subscription)
+
+from .models import Video
+
+post_save.connect(post_save_subscription,
+                  sender=Video,
+                  dispatch_uid="video_post_save")
+
+post_delete.connect(post_delete_subscription,
+                    sender=Video,
+                    dispatch_uid="video_post_delete")
