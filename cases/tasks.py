9912f2a98657e0e8e9c2b6d6b78b038345e2b371
Sangwoo Han <realappsdev@gmail.com>
2020-01-21 13:20:52 +0900

    Add improve_resolution task

--- cases/tasks.py
+++ cases/tasks.py
@@ -1,3 +1,4 @@
+import os
 import asyncio
 import json
 import websockets
@@ -5,10 +6,11 @@ import websockets
 from asgiref.sync import sync_to_async
 from celery import shared_task
 
+from django.core.files.base import File
 from django.db import close_old_connections
 from django.conf import settings
 
-from .models import Video
+from .models import Video, Image
 
 
 @shared_task
@@ -16,8 +18,17 @@ def create_gallery(video_id):
     print('create_gallery')
     video = Video.objects.get(id=video_id)
     path = str(video.upload)
-    print(f"video_path: {path}")
-    # asyncio.get_event_loop().run_until_complete(create_gallery_async(video_id, path))
+    asyncio.get_event_loop().run_until_complete(create_gallery_async(video_id, path))
+
+
+@shared_task
+def improve_resolution(image_id, obj_type, location):
+    print('improve_resolution')
+    image = Image.objects.get(id=image_id)
+    path = str(image.original)
+    asyncio.get_event_loop().run_until_complete(
+        improve_resolution_async(image_id, obj_type, location, path),
+    )
 
 
 async def create_gallery_async(video_id, path):
@@ -30,14 +41,6 @@ async def create_gallery_async(video_id, path):
             },
         }
 
-        # data = {
-        #     'action': 'image_resolution',
-        #     'type': 'human',  # ['human', 'vehicle_registration_plate']
-        #     'image': {
-        #         'id': 10,
-        #         'path': 'data/case/3/video/1/image/10/image.jpg',
-        #     },
-        # }
         await websocket.send(json.dumps(data))
         response = json.loads(await websocket.recv())
 
@@ -47,3 +50,28 @@ async def create_gallery_async(video_id, path):
 
         video.is_preprocessed = True
         await sync_to_async(video.save)()
+
+
+async def improve_resolution_async(image_id, obj_type, location, path):
+    async with websockets.connect(settings.WEBSOCKET_SERVER) as websocket:
+        data = {
+            'action': 'improve_resolution',
+            'type': obj_type,
+            'image': {
+                'id': image_id,
+                'location': location,
+                'path': path,
+            },
+        }
+
+        await websocket.send(json.dumps(data))
+        response = json.loads(await websocket.recv())
+
+    if response['ok']:
+        await sync_to_async(close_old_connections)()
+        image = await sync_to_async(Image.objects.get)(id=response['image_id'])
+
+        image_path = os.path.join(settings.MEDIA_ROOT, response['path'])
+        image.improvement = File(open(image_path, 'rb'), name='improvement.jpg')
+
+        await sync_to_async(image.save)()
